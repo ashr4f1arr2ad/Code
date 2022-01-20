6d877854030e99ab07e178f3c5bc5f061abe4d11
Ashrafi Arraad <44719020+ashrafiarraad@users.noreply.github.com>
2022-01-20 22:28:49 +0600

    Last index of array added

---
+++ lastIndexOfArray.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+    int array[] = {5, 65, 95, 2, 23, 45, 85, 46};
+    int lastIndexOfArray = sizeof(array)/sizeof(array[0]);
+    cout<<"Last Index Of Array Is: "<<lastIndexOfArray;
+}
